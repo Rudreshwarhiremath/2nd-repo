fabfa156d333ea8f4073944e9cfad456a4ea8b2f
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-12 19:57:46 +0530

    this is the mile Stone class it contains static method and its has return

---
+++ Milestone.java
@@ -0,0 +1,62 @@
+class Milestone{
+	static void showTheResponsibility(){
+		System.out.println("Responsiblity of humanbeing");
+	int  age = 45;
+		 char gender='F';
+        if (age==10){ 
+		System.out.println("go to the school");
+		return;}	
+		if(age==16){
+			System.out.println("go to the college 10+2");
+			
+		return;}
+		if(age==18){
+			System.out.println("getting voter id and DL");
+			if(gender=='F')
+			{System.out.println("girls can marry legeally");}
+          	else	
+			{System.out.println("boys not vallied for marry");}
+		return;}
+		if(age==21){
+			System.out.println("boys can marry legeally");
+			System.out.println("can drink");
+			
+		return;}
+        if(age==22  || age==23){
+			System.out.println("getting graduatte");
+			
+		return;}
+	     if(age==23  || age==25)
+	       {
+			System.out.println("searching job with having job");
+		return;}		
+		if(age==28){
+			System.out.println("if married getting kids");
+			
+		return;}
+		if(age==30){
+			System.out.println("having another kid");
+			
+		return;}
+		if(age==45){
+			System.out.println("Thinking about kids future");
+			
+		return;}
+		if(age==50){
+			System.out.println("kids educatation");
+			
+		return;}
+		if(age==58){
+			System.out.println("getting retired");
+			return;}
+		if(age==60){
+			System.out.println("thinking grand kids");
+			return;}
+		if(age==70){
+			System.out.println("rest in peace");
+		    return;	}
+			}
+			}
+			
+		
+	
