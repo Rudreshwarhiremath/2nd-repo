9ff8b89afab3d454a1e65a2bbe101639fb23ccb5
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-14 18:32:46 +0530

    class type

---
+++ MobileNo.java
@@ -0,0 +1,48 @@
+class MobileNo{
+   static String getNameByMobile(long number)
+   {
+	   System.out.println("getNameByMobile");
+   if (number==8088928928L)
+   {
+	   System.out.println("This no is Rudreshwar");
+	   return "Rudreshwar";
+   }
+  if (number==9482540194L){
+	  System.out.println("This no is Somu");
+	   return "Somu";  
+  }
+  if (number==9448928928L){
+	  System.out.println("This no is Appaji");
+	   return "Appaji";  
+  }
+  if (number==7676928928L){
+	  System.out.println("This no is Amma");
+	   return "Amma";  
+  }
+  if (number==9999999999L){
+	  System.out.println("This no is Prashant");
+	   return "Prashant";  
+  }
+  if (number==8888888888L){
+	  System.out.println("This no is Akhilesh");
+	   return "Akhilesh";  
+  }
+  if (number==7777777777L){
+	  System.out.println("This no is Veeru");
+	   return "Veeru";  
+  }
+  if (number==6666666666L){
+	  System.out.println("This no is Raviteja");
+	   return "Somu";  
+  }
+  if (number==6666655555L){
+	  System.out.println("This no is Nagu");
+	   return "Nagu";  
+  }
+  if (number==9999988888L){
+	  System.out.println("This no is Malak");
+	   return "Malak";  
+  }
+  return "Name";  
+}
+ }
