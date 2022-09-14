ee3214ec3320add5d5024d4fa7ebfe619fbaff35
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-14 07:55:24 +0530

    class type

---
+++ Identity.java
@@ -0,0 +1,69 @@
+class Identity{
+	static void printName(String name)
+	{
+		System.out.println("entred printName");
+		System.out.println("Name entred:"+name);
+    }
+	static void printNameAndEmail(String name,String Email)
+	{
+		System.out.println("Name entred:"+name);
+		System.out.println("Email entred:"+Email);
+    }
+	static void printNameAndMobile(String name,double mobile)
+	{
+		System.out.println("Name entred:"+name);
+		System.out.println("mobile entred:"+mobile);
+	}
+	static void printNameAndAge(String name,int Age)
+	{
+	System.out.println("Name entred:"+name);
+	System.out.println("Age entred:"+Age);
+		}
+	static void printNameAndAddress(String name,String Address)
+	{
+	System.out.println("Name entred:"+name);	
+	System.out.println("Address entred:"+Address);	
+	}
+	static void printNameAndEmailAndAddressAndAge(String name, String Email,String Address,int Age)
+	{
+	System.out.println("Name entred:"+name);
+	System.out.println("Email entred:"+Email);
+	System.out.println("Address entred:"+Address);
+    System.out.println("Age entred:"+Age);		
+	}
+	static void printNameAndGender(String name,char Gender)
+	{
+	System.out.println("Name entred:"+name);	
+	System.out.println("Gender:"+Gender);
+	}
+	static void printNameAndWorking(String name,String Working)
+	{
+	System.out.println("Name entred:"+name);	
+	System.out.println("Working as:"+Working);	
+	}
+	static void printNameAndShirtSize(String name, char Size)
+    {
+	System.out.println("Name entred:"+name);	
+	System.out.println("Size entred:"+Size);	
+	}	
+	static void printNameAndPantsize(String name, int size)
+	{
+	System.out.println("Name entred:"+name);	
+	System.out.println("Size entred:"+size);	
+	}
+	static void printNameAndWeightAndHeight(String name,int weight,double height)
+	{
+	System.out.println("Name entred:"+name);	
+    System.out.println("Weight entred:"+weight);	
+	System.out.println("Height entred:"+height);	
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
