15e4896783ac1feee836f718ebaefeb848c93903
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-14 18:35:21 +0530

    class type

---
+++ StateCaptial.java
@@ -0,0 +1,154 @@
+class StateCaptial{
+	static String getCapitalByState(String state) 
+	{
+	System.out.println("getCapitalByState");
+    if (state=="Karnataka")
+    {
+	System.out.println("Karnataka Capital city ");
+     return "Banglore";	
+	}
+if (state=="AndraPradesh")
+    {
+	System.out.println("AndraPradesh Capital city ");
+     return "Hydrabad";	
+	}
+if (state=="Telangana")
+    {
+	System.out.println("Telangana Capital city ");
+     return "Hydrabad";	
+	}
+if (state=="Maharastra")
+    {
+	System.out.println("Maharastra Capital city ");
+     return "Mubai";	
+	}
+if (state=="TamilNada")
+    {
+	System.out.println("TamilNada Capital city ");
+     return "Chennai";	
+	}
+if (state=="Keral")
+    {
+	System.out.println("Keral Capital city ");
+     return "Tiruvanantpuram";	
+	}
+if (state=="Goa")
+    {
+	System.out.println("Goa Capital city ");
+     return "Panaji";	
+	}	
+		if (state=="Odissa")
+    {
+	System.out.println("Odissa Capital city ");
+     return "Bhuvaneshwar";	
+	}
+	if (state=="Uttarpradesh")
+    {
+	System.out.println("Uttarpradesh Capital city ");
+     return "Lacknow";	
+	}
+	if (state=="MadhyaPradesh")
+    {
+	System.out.println("MadhyaPradesh Capital city ");
+     return "Bhupal";	
+	}
+	if (state=="Gujarat")
+    {
+	System.out.println("Gujarat Capital city ");
+     return "GandhiNagar";	
+	}
+	if (state=="Rajastana")
+    {
+	System.out.println("Rajastana Capital city ");
+     return "Jaipur";	
+	}
+	if (state=="Panjab")
+    {
+	System.out.println("Panjab Capital city ");
+     return "Chandigarh";	
+	}
+	if (state=="Bihar")
+    {
+	System.out.println("Bihar Capital city ");
+     return "Patna";	
+	}
+	if (state=="Hariyana")
+    {
+	System.out.println("Hariyana Capital city ");
+     return "Chandigarh";	
+	}
+	if (state=="Assam")
+    {
+	System.out.println("Assam Capital city ");
+     return "Dispur";	
+	}
+	if (state=="Jharkhanad")
+    {
+	System.out.println("Jharkhanad Capital city ");
+     return "Ranchi";	
+	}
+	if (state=="Uttarakhanda")
+    {
+	System.out.println("Uttarakhanda Capital city ");
+     return "Dehradun";	
+	}
+	if (state=="WestBengal")
+    {
+	System.out.println("WestBengal Capital city ");
+     return "Kolkata";	
+	}
+	if (state=="Chhattisgarh")
+    {
+	System.out.println("Chhattisgarh Capital city ");
+     return "Raipur";	
+	}
+	if (state=="HimachalPradesh")
+    {
+	System.out.println("HimachalPradesh Capital city ");
+     return "Shimla";	
+	}
+	if (state=="Nagaland")
+    {
+	System.out.println("Nagaland Capital city ");
+     return "Kohima";	
+	}
+	if (state=="Sikkim")
+    {
+	System.out.println("Sikkim Capital city ");
+     return "Gangtok";	
+	}
+	if (state=="Manipur")
+    {
+	System.out.println("Manipur Capital city ");
+     return "Imphal";	
+	}
+	if (state=="Meghalaya")
+    {
+	System.out.println("Meghalaya Capital city ");
+     return "Shimla";	
+	}
+	if (state=="Arunachalapradesh")
+    {
+	System.out.println("Arunachalapradesh Capital city ");
+     return "Itanagar";	
+	}
+	if (state=="Tripur")
+    {
+	System.out.println("Tripur Capital city ");
+     return "Agratal";	
+	}
+	if (state=="Mizoram")
+    {
+	System.out.println("Mizoram Capital city ");
+     return "Aizwal";
+	}
+    if (state=="Jammu&Kashmir")
+    {
+	System.out.println("Jammu&Kashmir Capital city ");
+     return "Srinagr";	
+	}	 
+	return null;	
+	}
+}
+
+ 
