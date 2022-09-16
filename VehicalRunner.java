34118ccb7ac5637d4979d39f91d6841cb26bd850
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-16 20:26:59 +0530

    main method

---
+++ VehicalRunner.java
@@ -0,0 +1,16 @@
+class VehicalRunner{
+	public static void main(String []rudra)
+	{
+		System.out.println("Enterd Source Destination");
+		Vehical.Transport("Basavakalyan","Hubbali");
+		System.out.println("Enterd Source Destination");
+		Vehical.Transport("Bidar","Banglore",5.00);
+		System.out.println("Enterd Source Destination");
+	    Vehical.Transport("Belagavi");
+		System.out.println("Enterd Source Destination");
+		Vehical.Transport("Belagavi",6.30);
+		System.out.println("Enterd Source Destination");
+		Vehical.Transport(7.30);
+	}
+	
+}
