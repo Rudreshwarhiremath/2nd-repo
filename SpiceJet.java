f69a453072d13943e7f94e90f31078071c24f40f
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-19 19:52:43 +0530

    class type

---
+++ SpiceJet.java
@@ -0,0 +1,12 @@
+class SpiceJet{
+	static void flightNos(String[]Flights){
+	System.out.println("Flights ready");
+	System.out.println("no of Flights:="+Flights.length);
+	for(int index=0;index<Flights.length;index++)
+	{
+    String ref=Flights[index];
+    System.out.println("Flights no:"+ref);	
+	}
+	System.out.println("Flights takeup");		
+	}
+}
