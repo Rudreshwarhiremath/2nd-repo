294b2876ad20fc796a901cd2d6f064c5361789ae
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-20 22:27:13 +0530

    cardrunner type

---
+++ CardRunner.java
@@ -0,0 +1,13 @@
+class CardRunner{
+	public static void main(String[]somu)
+	{
+		Card.printDetails();
+		Card.type="Aadhar";
+		Card.heigth=20.4;
+		Card.width=15.4;
+		Card.price=100;
+		String[]colors={"orange","White","green","black"};
+		Card.colour=colors;
+		Card.printDetails();
+	}
+}
