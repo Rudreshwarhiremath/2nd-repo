9b23b7d563178aa588068d1e0555977db8e74585
rg hiremath <rudreshwar.xworkz@gmail.com>
2022-09-26 11:42:43 +0530

    23 sep

--- ProductInformatin.java
+++ ProductInformatin.java
@@ -1,17 +1,18 @@
 class ProductInformatin{
-	public static void main(String[]info)
+	public static void main(String[]info){
+	if(info.length==5)
 	{
-	    String productname=info[0];
-        String producttype=info[1];
-		String productprice=info[2];
-		String productquanity=info[3];
-        String productquality=info[4];
-            System.out.println("Product Name : "+ productname);//Pen
-            System.out.println("Product Type : "+ producttype);//Statinary
-            System.out.println("Product Price : "+ productprice);//25
-			System.out.println("Product Quanity  : "+ productquanity);//20
-			System.out.println("Product Quality  : "+ productquality);//best
-                 int convertedPrice=Integer.parseInt(productprice);
+	    String productName=info[0];
+        String productType=info[1];
+		String productPrice=info[2];
+		String productQuanity=info[3];
+        String productQuality=info[4];
+            System.out.println("Product Name : "+ productName);//Pen
+            System.out.println("Product Type : "+ productType);//Statinary
+            System.out.println("Product Price : "+ productPrice);//25
+			System.out.println("Product Quanity  : "+ productQuanity);//20
+			System.out.println("Product Quality  : "+ productQuality);//best
+                 int convertedPrice=Integer.parseInt(productPrice);
                  System.out.println("Converted price: "+convertedPrice);
                     if(convertedPrice<=25)
                     {
@@ -21,7 +22,7 @@ class ProductInformatin{
                     else{
                     System.out.println("Pen is too costly");
                         }
-				    int convertedQuantity=Integer.parseInt(productquanity);
+				    int convertedQuantity=Integer.parseInt(productQuanity);
                     System.out.println("Converted Quantity: "+convertedQuantity);
 	                if(convertedQuantity<=20)
 					{
@@ -33,4 +34,5 @@ class ProductInformatin{
 				   int total=convertedPrice*convertedQuantity;//25*20=500
 				  System.out.println("Total price:"+total);//500
 				   	}
+	}
 }
